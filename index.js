6e66037487f19a5bab0bfc5efb366511c2627a7a
Steve Meservey <stevendmeservey@gmail.com>
2017-09-10 20:29:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function theBeatlesPlay (musicians, instruments){
+  var array = []
+
+  for (let i = 0, l = musicians.length; i < l; i++){
+    arry.push(`${musicians} plays ${instruments}`)
+  }
+  return array
+}
